6a263e04efe82d66d7b709dd0ced150b3d528239
Zydhan Linnar Putra <zydhanlinnar11@gmail.com>
2022-06-11 19:39:21 +0700

    refactor: move categories to separate file

---
+++ src/data/Categories.ts
@@ -0,0 +1,20 @@
+const categories = [
+  {
+    id: 1,
+    name: 'Urgent dan penting',
+  },
+  {
+    id: 2,
+    name: 'Urgent dan tidak penting',
+  },
+  {
+    id: 3,
+    name: 'Tidak urgent dan penting',
+  },
+  {
+    id: 4,
+    name: 'Tidak urgent dan tidak penting',
+  },
+];
+
+export default categories;
--- src/screens/CategoriesScreen.tsx
+++ src/screens/CategoriesScreen.tsx
@@ -2,25 +2,7 @@ import React from 'react';
 import {StatusBar, StyleSheet, Text, View} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import ListItem from '../components/ListItem';
-
-const categories = [
-  {
-    id: 1,
-    name: 'Urgent dan penting',
-  },
-  {
-    id: 2,
-    name: 'Urgent dan tidak penting',
-  },
-  {
-    id: 3,
-    name: 'Tidak urgent dan penting',
-  },
-  {
-    id: 4,
-    name: 'Tidak urgent dan tidak penting',
-  },
-];
+import categories from '../data/Categories';
 
 const CategoriesScreen = () => {
   return (
